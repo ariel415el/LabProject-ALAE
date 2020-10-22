Fixed numerical issues in Analytical PCA; Correct normalization of data
Added sklearn PCA

Linear_encoding/linear_autoencoders.py
@@ -1,6 +1,9 @@
 from autoencoders import *
 from utils import plot_training
 
+from sklearn.decomposition import PCA
+from scipy.linalg import eigh
+
 
 class LinearAutoEncoder(EncoderDecoder):
     def __init__(self, data_dim, laten_dim):
@@ -8,11 +11,11 @@ class LinearAutoEncoder(EncoderDecoder):
         self.restoration_matrix = None
         self.projection_matrix = None
 
-    def encode(self, zero_mean_data):
-        return np.dot(zero_mean_data, self.projection_matrix)
+    def encode(self, data):
+        return np.dot(data, self.projection_matrix)
 
-    def decode(self, zero_mean_data):
-        return np.dot(zero_mean_data, self.restoration_matrix)
+    def decode(self, data):
+        return np.dot(data, self.restoration_matrix)
 
     def get_reconstuction_loss(self, zero_mean_data):
         return np.linalg.norm(zero_mean_data - self.decode(self.encode(zero_mean_data)), ord=2) / zero_mean_data.shape[0]
@@ -29,26 +32,64 @@ class AnalyticalPCA(LinearAutoEncoder):
     def __init__(self, data_dim, latent_dim):
         super(AnalyticalPCA, self).__init__(data_dim, latent_dim)
         self.name = "AnalyticalPCA"
+        self.train_mean = None
 
-    def learn_encoder_decoder(self, data, plot_path=None):
+    def learn_encoder_decoder(self, train_samples, plot_path=None):
         """
         Perform PCA by triming the result orthonormal transformation of SVD
         Assumes X is zero centered
         """
         start = time()
         print("\tLearning encoder decoder... ",end="")
+        self.train_mean = train_samples.mean(0)
+        data = train_samples - self.train_mean
 
-        CovMat = np.dot(data.transpose(),data)
+        CovMat = np.dot(data.transpose(), data)
 
-        vals, vecs = np.linalg.eigh(CovMat)
+        # vals_np, vecs_np = np.linalg.eigh(CovMat)
+        # # Take rows corresponding to highest eiegenvalues
+        # order = np.argsort(vals_np,)[::-1][:self.latent_dim]
+        # self.projection_matrix = vecs_np[order].transpose()
+        # self.restoration_matrix = vecs_np[order]
 
-        # Take rows corresponding to highest eiegenvalues
-        order = np.argsort(vals,)[::-1][:self.latent_dim]
-
-        self.projection_matrix = vecs[order].transpose()
-        self.restoration_matrix = vecs[order]
+        vals, vecs = eigh(CovMat, subset_by_index=[self.data_dim - self.latent_dim, self.data_dim - 1])
+        self.projection_matrix = vecs
+        self.restoration_matrix = vecs.transpose()
 
         print(f"Finished in {time() - start:.2f} sec")
+
+    def encode(self, data):
+        zero_mean_data = data - self.train_mean
+        return super(AnalyticalPCA, self).encode(zero_mean_data)
+
+    def decode(self, features):
+        data = super(AnalyticalPCA, self).decode(features)
+        return data + self.train_mean
+
+
+class SKLearnPCA(LinearAutoEncoder):
+    def __init__(self, data_dim, latent_dim):
+        super(SKLearnPCA, self).__init__(data_dim, latent_dim)
+        self.pca = PCA(n_components=latent_dim)
+        self.name = "SKLearnPCA"
+
+    def learn_encoder_decoder(self, train_samples, plot_path=None):
+        self.pca.fit(train_samples)
+
+    def encode(self, data):
+        if len(data.shape) == 1:
+            return self.pca.transform([data])[0]
+        else:
+            return self.pca.transform(data)
+
+    def decode(self, features):
+        if len(features.shape) == 1:
+            return self.pca.inverse_transform([features])[0]
+        else:
+            return self.pca.inverse_transform(features)
+
+    def get_orthonormality_loss(self):
+        return 0
 
 
 class NumericMinimizationPCA(LinearAutoEncoder):
@@ -57,9 +98,11 @@ class NumericMinimizationPCA(LinearAutoEncoder):
         self.optimization_steps = optimization_steps
         self.regularization_factor = regularization_factor
         self.lr = lr
+        self.train_mean = None
         self.name = f"NumericMinimizationPCA_s[{optimization_steps}]_lr[{lr}]_r[{regularization_factor}]"
 
-    def learn_encoder_decoder(self, data, plot_path=None):
+
+    def learn_encoder_decoder(self, train_samples, plot_path=None):
         """
         Perform PCA by a straightforward minimization of ||X - XCC^T|| constraint to C's columns being orthonormal vectors
         i.e C^TC = I.
@@ -69,6 +112,9 @@ class NumericMinimizationPCA(LinearAutoEncoder):
         see http://alexhwilliams.info/itsneuronalblog/2016/03/27/pca/
         Assumes X is zero centered
         """
+        self.train_mean = train_samples.mean(0)
+        data = train_samples - self.train_mean
+
         X = torch.tensor(data, requires_grad=False)
         C = torch.tensor(np.random.normal(0, 1, (data.shape[1], self.latent_dim)), requires_grad=True)
         optimizer = torch.optim.Adam([C], lr=self.lr)
@@ -111,6 +157,14 @@ class NumericMinimizationPCA(LinearAutoEncoder):
         self.projection_matrix = C[:, order]
         self.restoration_matrix = self.projection_matrix.transpose()
 
+    def encode(self, data):
+        data -= self.train_mean
+        return super(NumericMinimizationPCA, self).encode(data)
+
+    def decode(self, features):
+        data = super(NumericMinimizationPCA, self).decode(features)
+        return data + self.train_mean
+
 
 class LinearVanilaAE(LinearAutoEncoder):
     def __init__(self, data_dim, laten_dim, optimization_steps=1000, lr=0.001, batch_size=64, metric='l2'):
@@ -119,12 +173,24 @@ class LinearVanilaAE(LinearAutoEncoder):
                                    lr=lr, batch_size=batch_size, mode="Linear", metric=metric)
 
         self.name = "Linear-" + self.VanillaVAE.name
+        self.train_mean = None
 
-    def learn_encoder_decoder(self, data, plot_path=None):
+    def learn_encoder_decoder(self, train_samples, plot_path=None):
+        self.train_mean = train_samples.mean(0)
+        data = train_samples - self.train_mean
+
         self.VanillaVAE.learn_encoder_decoder(data, plot_path)
 
         self.projection_matrix = self.VanillaVAE.E.weight.t().detach().numpy()
         self.restoration_matrix = self.VanillaVAE.D.weight.t().detach().numpy()
+
+    def encode(self, data):
+        zero_mean_data = data - self.train_mean
+        return super(LinearVanilaAE, self).encode(zero_mean_data)
+
+    def decode(self, features):
+        data = super(LinearVanilaAE, self).decode(features)
+        return data + self.train_mean
 
 
 class LinearALAE(LinearAutoEncoder):
@@ -134,26 +200,50 @@ class LinearALAE(LinearAutoEncoder):
                          optimization_steps=optimization_steps, lr=lr, batch_size=batch_size)
 
         self.name = "Linear-" + self.ALAE.name
+        self.train_mean = None
 
-    def learn_encoder_decoder(self, data, plot_path=None):
+    def learn_encoder_decoder(self, train_samples, plot_path=None):
+        self.train_mean = train_samples.mean(0)
+        data = train_samples - self.train_mean
+
         self.ALAE.learn_encoder_decoder(data, plot_path)
 
         self.projection_matrix = self.ALAE.E.weight.t().detach().numpy()
         self.restoration_matrix = self.ALAE.G.weight.t().detach().numpy()
 
+    def encode(self, data):
+        zero_mean_data = data - self.train_mean
+        return super(LinearALAE, self).encode(zero_mean_data)
+
+    def decode(self, features):
+        data = super(LinearALAE, self).decode(features)
+        return data + self.train_mean
+
 
 class LinearLatentRegressor(LinearAutoEncoder):
-    def __init__(self, data_dim, laten_dim, optimization_steps=1000, lr=0.002, batch_size=64, regressor_training='joint'):
+    def __init__(self, data_dim, laten_dim, optimization_steps=1000, lr=0.005, batch_size=64, regressor_training='joint'):
         super(LinearLatentRegressor, self).__init__(data_dim, laten_dim)
         self.LatentRegressor = LatentRegressor(data_dim, laten_dim, mode="Linear",
                                                optimization_steps=optimization_steps, lr=lr, batch_size=batch_size,
                                                regressor_training=regressor_training)
 
         self.name = "Linear-" + self.LatentRegressor.name
+        self.train_mean = None
 
-    def learn_encoder_decoder(self, data, plot_path=None):
+    def learn_encoder_decoder(self, train_samples, plot_path=None):
+        self.train_mean = train_samples.mean(0)
+        data = train_samples - self.train_mean
+
         self.LatentRegressor.learn_encoder_decoder(data, plot_path)
 
         self.projection_matrix = self.LatentRegressor.E.weight.t().detach().numpy()
         self.restoration_matrix = self.LatentRegressor.G.weight.t().detach().numpy()
 
+
+    def encode(self, data):
+        zero_mean_data = data - self.train_mean
+        return super(LinearLatentRegressor, self).encode(zero_mean_data)
+
+    def decode(self, features):
+        data = super(LinearLatentRegressor, self).decode(features)
+        return data + self.train_mean

autoencoders.py
@@ -5,29 +5,29 @@ from utils import plot_training
 import torch.nn.functional as F
 from time import time
 
-class BiGanMLP(nn.Module):
-    def __init__(self, input_shape, hidden_dim=1024):
-        super(BiGanMLP, self).__init__()
-        self.input_shape = input_shape
-        self.fc1 = nn.Linear(input_shape, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
-        self.fc3 = nn.Linear(hidden_dim, 10)
-        self.droput = nn.BatchNorm2d
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = self.droput(x)
-        x = F.relu(self.fc2(x))
-        x = self.droput(x)
-        x = self.fc3(x)
-        x = F.log_softmax(x, dim=1)
-        return x
+# class BiGanMLP(nn.Module):
+#     def __init__(self, input_shape, hidden_dim=1024):
+#         super(BiGanMLP, self).__init__()
+#         self.input_shape = input_shape
+#         self.fc1 = nn.Linear(input_shape, hidden_dim)
+#         self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+#         self.fc3 = nn.Linear(hidden_dim, 10)
+#         self.droput = nn.BatchNorm2d
+#
+#     def forward(self, x):
+#         x = F.relu(self.fc1(x))
+#         x = self.droput(x)
+#         x = F.relu(self.fc2(x))
+#         x = self.droput(x)
+#         x = self.fc3(x)
+#         x = F.log_softmax(x, dim=1)
+#         return x
 
 
 class EncoderDecoder(object):
     def __init__(self, data_dim, latent_dim):
         self.name = "AbstractEncoderDecoder"
-        self.data_dimdata_dim= data_dim
+        self.data_dim =  data_dim
         self.latent_dim = latent_dim
 
     def __str__(self):
@@ -132,8 +132,8 @@ class ALAE(EncoderDecoder):
             self.F = torch.nn.Linear(self.z_dim, latent_dim, bias=False)
             self.G = torch.nn.Linear(latent_dim, data_dim, bias=False)
             self.E = torch.nn.Linear(data_dim, latent_dim, bias=False)
-            self.D = torch.nn.Linear(latent_dim, 1, bias=False)
-            # self.D = nn.Sequential(nn.Linear(latent_dim, 1), nn.Sigmoid())
+            # self.D = torch.nn.Linear(latent_dim, 1, bias=False)
+            self.D = nn.Sequential(nn.Linear(latent_dim, 1), nn.Sigmoid())
         else:
             raise Exception("Mode no supported")
 

utils.py
@@ -63,32 +63,35 @@ def visualize_classification(data, predictions, labels, plot_path):
     plt.clf()
 
 
-def plot_latent_interpolation(ae, data, labels, N=5, plot_path=None):
+def plot_latent_interpolation(ae, data, N=5, plot_path=None):
     im_dim = int(np.sqrt(len(data[0])))
     os.makedirs(os.path.dirname(plot_path), exist_ok=True)
-    # Sample interpollation extremes
-    # unique_labels = np.unique(labels)
-    # different_labels = np.random.choice(unique_labels, 2, replace=False)
-    # np.random.choice()
-    start = data[0]
-    end = data[-1]
+    indices = np.random.randint(len(data), size=2)
+    start = data[indices[0]]
+    end = data[indices[1]]
 
     start_z = ae.encode(start)
     end_z = ae.encode(end)
 
     fig = plt.figure()
-    ax = fig.add_subplot(2, N, 1)
-    ax.imshow(start.reshape(im_dim, im_dim))
-    ax = fig.add_subplot(2, N, N)
-    ax.imshow(end.reshape(im_dim, im_dim))
+    ax = fig.add_subplot(3, N, 1)
+    ax.imshow(start.reshape(im_dim, im_dim), cmap='gray')
+    ax = fig.add_subplot(3, N, N)
+    ax.imshow(end.reshape(im_dim, im_dim), cmap='gray')
     for i, c in enumerate(np.arange(0, 1 + 1 / N, 1 / (N-1))):
         interpollated_z = (1-c) * start_z + c * end_z
-        img = ae.decode(interpollated_z)
-        ax = fig.add_subplot(2, N, N + 1 + i)
-        ax.set_title(str(c))
-        ax.imshow(img.reshape(im_dim, im_dim))
+        img = np.clip(ae.decode(interpollated_z), 0 , 1)
+        ax = fig.add_subplot(3, N, N + 1 + i)
+        ax.set_title(f"Latent {c}")
+        ax.imshow(img.reshape(im_dim, im_dim), cmap='gray')
         ax.set_xticks([])
         ax.set_yticks([])
 
+        interpollated_img = (1-c) * start + c * end
+        ax = fig.add_subplot(3, N, 2*N + 1 + i)
+        ax.set_title(f"Data {c}")
+        ax.imshow(interpollated_img.reshape(im_dim, im_dim), cmap='gray')
+        ax.set_xticks([])
+        ax.set_yticks([])
     plt.savefig(plot_path)
     plt.clf()